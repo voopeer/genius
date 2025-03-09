2dd5e8ecf77bfbdcc5156392cdcb27dbbf408458
voopeer <151393232+voopeer@users.noreply.github.com>
2025-03-09 22:23:18 +0200

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
-const modalBtnClose = document.querySelector('.modal-btn-close');
+const modalBtnClose = document.querySelector('.modal-close-btn');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
